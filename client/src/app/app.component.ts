735a4a071652632d683db6c0ac6d67feef277c59
carlosjuliojr <carlosjuliojr@hotmail.com>
2017-09-22 22:24:03 -0400

    changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,17 +2,17 @@
 
 @Component({
     moduleId: module.id,
-    selector: 'app',
+    selector:'app',
     templateUrl: 'app.component.html',
     styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{ 
+export class AppComponent implements OnInit{
 
 	ngOnInit() {
 		navigator.geolocation.getCurrentPosition(position => {
         // this.location = position.coords;
-        console.log(position.coords); 
+        console.log(position.coords);
       });
     }
 
